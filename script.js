c3f6690e9c4dd42d1808c49b9fd41c2cb62a1118
Christopher Wong <christopherwong@Christophers-MacBook-Pro.local>
2025-11-18 18:27:36 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU'],
+        datasets: [{
+            data: [12, 19, 3, 5, 2, 3, 4],
+            backgroundColor: 'rgba(75, 192, 192, 1)',
+            borderRadius: 10
+        }]
+    },
+    options: {
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                grid: {
+                    display: false
+                }
+            },
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});

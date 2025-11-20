d827c0caa4c44819c27b38daf0e563fcd154a265
Christopher Wong <christopherwong49@gmail.com>
2025-11-19 22:11:39 -0500

    added table

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
 // Declare chart
-const ctx = document.getElementById('myChart');
+const ctx1 = document.getElementById('patient-one');
+const ctx2 = document.getElementById('patient-two');
 
-new Chart(ctx, {
+new Chart(ctx1, {
     type: 'bar',
     data: {
-        labels: ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU'],
+        labels: ['0', '4', '8', 'TH', 'FR', 'SA', 'SU'],
         datasets: [{
             data: [12, 19, 3, 5, 2, 3, 4],
-            backgroundColor: 'rgba(75, 192, 192, 1)',
+            backgroundColor: 'rgb(92, 166, 203)',
             borderRadius: 10
         }]
     },
     options: {
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                grid: {
+                    display: false
+                }
+            },
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+new Chart(ctx2, {
+    type: 'bar',
+    data: {
+        labels: ['0', '4', '8', 'TH', 'FR', 'SA', 'SU'],
+        datasets: [{
+            data: [12, 19, 3, 5, 2, 3, 4],
+            backgroundColor: 'rgb(92, 166, 203)',
+            borderRadius: 10
+        }]
+    },
+    options: {  
         plugins: {
             legend: {
                 display: false
